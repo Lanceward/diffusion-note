dd190831ce5aa1e076f3a95da7a8d7f0f542a64d
Lanceward <27809000+Lanceward@users.noreply.github.com>
2026-04-22 00:32:27 +0800

    test and train scripts

---
+++ generate.py
@@ -0,0 +1,67 @@
+import torch
+from diffusion_model import SimpleUNetModel
+import matplotlib.pyplot as plt
+
+DIGIT = 7
+
+if __name__=="__main__":
+    T = 1000
+    beta_1 = 1e-4
+    beta_T = 0.02
+    model_path = "./logs/SimpleUNetModel_T1000_b128_lr2e-05_mnist/checkpoint_epoch_28.pth"
+    DEV = "mps"
+    model = SimpleUNetModel(
+        sample_size=28, 
+        in_channels=1, 
+        out_channels=1, 
+        dropout=0.1, 
+        num_class_embeds=10, 
+        down_block_types=(
+            "DownBlock2D",
+            "AttnDownBlock2D",
+            "AttnDownBlock2D",
+        ),
+        up_block_types=(
+            "AttnUpBlock2D",
+            "AttnUpBlock2D",
+            "UpBlock2D",
+        ),
+        block_out_channels=(32, 64, 64),
+    ).to(DEV)
+    model.load_state_dict(torch.load(model_path, weights_only=False, map_location=DEV)['net'])
+    model.eval()
+    # print(model)
+    
+    # define the scheduler parameters
+    # here for schedules, parameter_t is stored at index t
+    beta = torch.zeros(T+1).to(DEV)
+    alpha = torch.zeros(T+1).to(DEV)
+    alpha_hat = torch.zeros(T+1).to(DEV)
+    for t in range(1, T+1):
+        # parameters ranges from 1 to T
+        beta[t] = (beta_T-beta_1)/(T-1)*(t-1)+beta_1
+        alpha[t] = 1-beta[t]
+        if t == 1:
+            alpha_hat[t] = alpha[t]
+        else:
+            alpha_hat[t] = alpha_hat[t-1]*alpha[t]
+    
+    x_t = torch.normal(mean=torch.zeros(1, 1, 28, 28), std=torch.ones(1, 1, 28, 28)).to(DEV) # x at 1000
+    class_label = torch.tensor(DIGIT, device=DEV)
+    for t in range(T, 0, -1):
+        print(f'\r {t}/{T} mean: {x_t.mean()}, min|max: {x_t.min()}|{x_t.max()}', end='      ')
+        if t > 1:
+            z = torch.normal(mean=torch.zeros(1, 1, 28, 28), std=torch.ones(1, 1, 28, 28)).to(DEV)
+        else:
+            z = torch.zeros((1, 1, 28, 28)).to(DEV)
+        
+        pred_noise = model.forward(sample=x_t, timestep=t, class_labels=class_label).sample
+        x_t_1 = 1/torch.sqrt(alpha[t])*(x_t - (1-alpha[t])/torch.sqrt(1-alpha_hat[t])*pred_noise) + torch.sqrt(beta[t])*z
+        x_t = x_t_1
+    print()
+    
+    # show the image
+    img_to_show = x_t.detach().cpu().squeeze(0, 1).numpy()
+    plt.imshow(img_to_show, cmap='gray')
+    plt.axis('off') # Optional: hides axes
+    plt.show()
--- train.py
+++ train.py
@@ -6,7 +6,7 @@ import argparse
 import numpy as np
 import torch
 import torch.nn.functional as F
-from torch.optim.lr_scheduler import LinearLR, ConstantLR, SequentialLR
+from torch.optim.lr_scheduler import LinearLR, ConstantLR, SequentialLR, CosineAnnealingLR, MultiStepLR
 from torch.utils.tensorboard import SummaryWriter
 from torchvision import datasets, transforms
 from diffusion_model import SimpleUNetModel
@@ -34,7 +34,7 @@ if __name__=='__main__':
                         help='number of total epochs to run')
     parser.add_argument('-out-dir', type=str, default='./logs', help='root dir for saving logs and checkpoint')
     parser.add_argument('-resume', type=str, help='resume from the checkpoint path')
-    parser.add_argument('-lr', default=2e-4, type=float, help='learning rate')
+    parser.add_argument('-lr', default=2e-5, type=float, help='learning rate')
 
     args = parser.parse_args()
     print(args)
@@ -60,7 +60,7 @@ if __name__=='__main__':
             "UpBlock2D",
         ),
         block_out_channels=(32, 64, 64),
-    )
+    ).to(args.device)
     
     # define the scheduler parameters
     # here for schedules, parameter_t is stored at index t
@@ -83,24 +83,24 @@ if __name__=='__main__':
     # Optimizer
     optimizer = torch.optim.Adam(net.parameters(), lr=args.lr)
     # Scheduler
-    warmup_scheduler = LinearLR(optimizer, start_factor=0.1, end_factor=1.0, total_iters=10)
-    main_scheduler = ConstantLR(optimizer, factor=1.0, total_iters=100)
-    scheduler = SequentialLR(
-        optimizer, 
-        schedulers=[warmup_scheduler, main_scheduler], 
-        milestones=[10]
-    )
+    # warmup_scheduler = LinearLR(optimizer, start_factor=0.1, end_factor=1.0, total_iters=10)
+    # main_scheduler = ConstantLR(optimizer, factor=1.0, total_iters=100)
+    # main_scheduler = CosineAnnealingLR(optimizer, T_max=100)
+    scheduler = MultiStepLR(optimizer, milestones=[25, 50, 75])
+    # scheduler = SequentialLR(
+    #     optimizer, 
+    #     schedulers=[warmup_scheduler, main_scheduler], 
+    #     milestones=[10]
+    # )
 
     # preparation around training
     start_epoch=0
-    max_test_acc=0.0
     if args.resume:
         checkpoint = torch.load(args.resume, map_location='cpu')
         net.load_state_dict(checkpoint['net'])
         optimizer.load_state_dict(checkpoint['optimizer'])
         scheduler.load_state_dict(checkpoint['scheduler'])
         start_epoch = checkpoint['epoch'] + 1
-        max_test_acc = checkpoint['max_test_acc']
     
     out_dir = os.path.join(args.out_dir, f'{type(net).__name__}_T{T}_b{args.b}_lr{args.lr}')
     out_dir += '_mnist'
@@ -119,25 +119,63 @@ if __name__=='__main__':
         args_txt.write(' '.join(sys.argv))
         
     #training starts
-    net = net.to(args.device)
+    dev = torch.device(args.device)
     alpha_hat = alpha_hat.to(args.device)
     for epoch in range(start_epoch, args.epochs):
+        start_time = time.time()
         net.train()
+        idx = 0
+        train_samples = 0
+        train_loss = 0.0
+        loss = 0.0
         for img, label in train_loader:
+            print("\r" + str(idx) + "/" + str(len(train_loader)) + ' ' + str(img.shape) + " loss " + str(loss.item()), end='')
+            idx+=1
             img = img.to(args.device)
             label = label.to(args.device)
-            print(img.shape, label.shape)
+            this_batch = img.shape[0]
+            # print(img.shape, label.shape, end='')
             
             # generate t and epsilon for each image in barch
-            ts = torch.randint(1, high=T+1, size=(args.b,)).to(args.device)
-            eps = torch.normal(mean=torch.zeros(args.b, 1, 28, 28), std=torch.ones(args.b, 1, 28, 28)).to(args.device)
-            print(ts.shape, eps.shape)
+            ts = torch.randint(1, high=T+1, size=(this_batch,)).to(args.device)
+            eps = torch.normal(mean=torch.zeros(this_batch, 1, 28, 28), std=torch.ones(this_batch, 1, 28, 28)).to(args.device)
+            # print(ts.shape, eps.shape, end='')
             
+            # add noise
             ahats = alpha_hat[ts]
             img_noised = torch.sqrt(ahats).view(-1, 1, 1, 1) * img + torch.sqrt(1-ahats).view(-1, 1, 1, 1) * eps
             
-            print(img_noised.shape, ts.shape, label.shape)
+            #forward            
+            pred_noise = net.forward(sample=img_noised, timestep=ts, class_labels=label).sample
+            # print(pred_noise.shape, end='')
+            loss = F.mse_loss(pred_noise, eps)
             
-            noises = net.forward(sample=img_noised, timestep=ts, class_labels=label)
-            exit()
+            #backward
+            loss.backward()
+            optimizer.step()
             
+            #statistics
+            train_samples += label.numel()
+            train_loss += loss.item() * label.numel()
+        print()
+
+        #epoch stats
+        scheduler.step()
+        train_time = time.time()
+        train_speed = train_samples / (train_time - start_time)
+        train_loss /= train_samples
+        
+        writer.add_scalar('train_loss', train_loss, epoch)
+        
+        checkpoint = {
+            'net': net.state_dict(),
+            'optimizer': optimizer.state_dict(),
+            'scheduler': scheduler.state_dict(),
+            'epoch': epoch,
+        }
+        
+        torch.save(checkpoint, os.path.join(out_dir, f'checkpoint_epoch_{epoch}.pth'))
+
+        print(args)
+        print(f'epoch ={epoch}, train_loss ={train_loss: .4f}')
+        
