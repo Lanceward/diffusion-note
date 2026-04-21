eee51c2651e2181e1d05d8499f142e0fa9708761
Lanceward <27809000+Lanceward@users.noreply.github.com>
2026-04-21 19:23:48 +0800

    some more prep works

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from torchvision import datasets, transforms
 
 transform = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize((0.1307,), (0.3081,))
+        transforms.Normalize((0.5,), (0.5,))
         ])
 
 dataset_train = datasets.MNIST(root="./data", train=True, download=True, transform=transform)
@@ -11,3 +11,5 @@ dataset_test = datasets.MNIST(root="./data", train=False, download=True, transfo
 
 train_loader = torch.utils.data.DataLoader(dataset_train)
 test_loader = torch.utils.data.DataLoader(dataset_test)
+
+
